da691080f8b12c24c067f58884cd130e76078ba0
Seonae Jang <84896212+seonae-j@users.noreply.github.com>
2024-06-10 11:54:14 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+S = int(input())
+
+if (90<= S <=100) :
+    print("A")
+elif (80<= S <=89) :
+    print("B")
+elif (70<= S <= 79) :
+    print("C")
+elif (60<= S <= 69) :
+    print("D")
+else :
+    print("F")
